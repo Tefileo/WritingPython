4db2daa95fea07cb95adf09bf74bc7cca03ce22e
Tefileo Stephens <TStephens@spartaglobal.com>
2020-02-04 14:56:53 +0000

    Python 101 file added and updated, along with hello world script

---
+++ Python101.py
@@ -0,0 +1,25 @@
+# Variables In Python
+    # A variable is a box. And we put stuff inside and give it a name.
+
+# Assign a variable
+# gave the box a name and put stuff inside
+
+    box_pillows = 'pillows and stuff'
+
+# look inside box
+
+    print(box_pillows)
+
+# Re assignment of a variable
+# changing the stuff inside the box
+
+    box_pillows = 'Books'
+    print(box_pillows)
+    box_pillows = 14
+    print(box_pillows)
+
+# Important functions in python
+# print()
+print('hello hello i am a string')
+print(box_pillows)
+print('14')
