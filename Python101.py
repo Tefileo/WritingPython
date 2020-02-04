e845183aa17342d3b2da238686d7e8897fd5df2f
Tefileo Stephens <TStephens@spartaglobal.com>
2020-02-04 15:29:21 +0000

    more python comments done

--- Python101.py
+++ Python101.py
@@ -1,37 +1,7 @@
-# Variables In Python
-    # A variable is a box. And we put stuff inside and give it a name.
-
-# Assign a variable
-# gave the box a name and put stuff inside
-
-    box_pillows = 'pillows and stuff'
-
-# look inside box
-
-    print(box_pillows)
-
-# Re assignment of a variable
-# changing the stuff inside the box
-
-    box_pillows = 'Books'
-    print(box_pillows)
-    box_pillows = 14
-    print(box_pillows)
-
-# Important functions in python
-# print()
-print('hello hello i am a string')
-print(box_pillows)
-print('14')
-
-my_string = "Amazing Grilled Fish"
-
-Joining of two strings
-
 first_name = 'Boris'
 last_name = 'May'
 
-print(first_name + + last)
 
-# Interpolation
-# You inject a string into another string
+print (first_name.upper())
+print (first_name.lower())
+print(first_name.split())
